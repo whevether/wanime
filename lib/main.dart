cf08e2135d3287fa48f1ec9badbe7c595302728e
wanwenfeng4798 <113097751+wanwenfeng4798@users.noreply.github.com>
2023-11-16 09:56:44 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ import 'package:wanime/models/db/novel_history.dart';
 import 'package:wanime/services/comic_download_service.dart';
 import 'package:wanime/services/novel_download_service.dart';
 import 'package:wanime/services/db_service.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:wanime/routes/app_pages.dart';
 import 'package:wanime/services/local_storage_service.dart';

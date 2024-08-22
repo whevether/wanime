5fea026c81bf1cc597ce0839c5c4bfd999dbf1c6
keep.wan <whevether@outlook.com>
2024-08-22 16:04:56 +0800

    更新android配置

--- lib/requests/common/custom_interceptor.dart
+++ lib/requests/common/custom_interceptor.dart
@@ -20,7 +20,7 @@ Request Query：${err.requestOptions.queryParameters}
 Request Data：${err.requestOptions.data}
 Request Headers：${err.requestOptions.headers}
 Response Headers：${err.response?.headers.map}
-Response Data：${err.response?.data}''', err.stackTrace ?? StackTrace.current);
+Response Data：${err.response?.data}''', err.stackTrace);
     super.onError(err, handler);
   }
 
